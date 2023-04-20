d08d7a39e2c3a64b32f61966b877b0f7661edc1a
Martin Nicolas Perez <manperez@fi.uba.ar>
2023-04-20 20:16:57 -0300

    Add endpoint /space_news returning first 5 titles

--- app/app.js
+++ app/app.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios');
 
 app.get('/space_news', (req, res) => {
-  res.send('TODO: Space News')
+  axios.get('https://api.spaceflightnewsapi.net/v4/articles')
+  .then(spaceFlightRes => {
+    const headerDate = spaceFlightRes.headers && spaceFlightRes.headers.date ? spaceFlightRes.headers.date : 'no response date';
+    console.log('Status Code:', spaceFlightRes.status);
+    console.log('Date in Response header:', headerDate);
+
+    const response_body = spaceFlightRes.data;
+    console.log('Data:', response_body);
+    const spaceflightNews = response_body.results.slice(0, 5);;
+    const titles = spaceflightNews.map((spaceflightNew) => spaceflightNew.title);
+    res.send(titles)
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+    res.send(err.message)
+  });
 })
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
-
-// The signals we want to handle
-// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
-var signals = {
-  'SIGHUP': 1,
-  'SIGINT': 2,
-  'SIGTERM': 15
-};
-// Do any necessary shutdown logic for our application here
-const shutdown = (signal, value) => {
-  console.log("shutdown gracefully!");
-  server.close(() => {
-    console.log(`server stopped by ${signal} with value ${value}`);
-    process.exit(0);
-  });
-};
-// Create a listener for each of the signals that we want to handle
-Object.keys(signals).forEach((signal) => {
-  process.on(signal, () => {
-    shutdown(signal, signals[signal]);
-  });
 });
